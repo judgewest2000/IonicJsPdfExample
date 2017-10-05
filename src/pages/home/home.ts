a31c492da518f716221455803eebe350d9321f6e
James West <judgewest2000@hotmail.co.uk>
2017-10-05 11:10:42 +0100

    some table injection

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,11 +13,35 @@ export class HomePage {
 
   }
 
+  content: { name: string, id: string, data: string }[] = [
+    { name: 'nikhill1', id: '1', data: 'temp1' },
+    { name: 'nikhill2', id: '2', data: 'temp2' }
+  ];
 
-  createPdf(){
+  createPdf() {
     const doc = new jsPDF()
-    
     doc.text('Hello world!', 10, 10)
+
+    const nameX = 10;
+    const idX = 40;
+    const dataX = 70;
+    const advanceY = 10;
+
+    doc.text('Name', nameX, 20);
+    doc.text('Id', idX, 20);
+    doc.text('Data', dataX, 20);
+
+    
+    let currentY = 30;
+
+    this.content.forEach(item =>{
+      doc.text(item.name, nameX, currentY);
+      doc.text(item.id, idX, currentY);
+      doc.text(item.data, dataX, currentY);
+
+      currentY = currentY + advanceY;
+    });
+
     doc.save('a4.pdf')
   }
 
